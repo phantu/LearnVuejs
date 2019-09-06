a4310cff817b3b07cbb3c6aabeddccf6e35cd5e8
Tu Phan <phantu21101998@gmail.com>
2019-09-06 14:46:48 +0700

    slected cpn

---
+++ learnwords/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <button @click="Slectedcpn='appCart'" class="btn btn-danger">Card</button>
+          <button @click="Slectedcpn='appAbout'" class="btn btn-danger">About</button>
+          <button @click="Slectedcpn='appContact'" class="btn btn-danger">Contact</button>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12">
+          <!-- <appCart cardDM="cart2" ><p slot="title">This is my car</p><p slot="content">This is my car 2</p></appCart> -->
+          <component :is="Slectedcpn"></component>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from '../src/component/Card';
+import Contact from '../src/component/Contact';
+import About from '../src/component/About';
+export default {
+  data:function(){
+    return{
+      Slectedcpn:'appCart'
+    }
+    
+  },
+  components:{
+    appCart : Card,
+    appContact : Contact,
+    appAbout : About
+  }
+}
+</script>
+
---
+++ learnwords/src/component/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div >
+    <h2>{{cardDM}}</h2>
+    <p>This is my car</p>
+    <slot name="content"></slot>
+    <slot name="title"></slot>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props:['cardDM']
+}
+</script>
+
+<style scoped>
+  div{
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px rgba(0, 0, 0, .6);
+    padding: 30px;
+    margin: 20px auto;
+    text-align: center;
+  }
+</style>
